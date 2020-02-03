ca89112ef069d47ae11da020b3b533f4a1ea840d
Andrei Cirlan <andreicirlan@andreis-air.home>
2020-02-03 15:57:45 +0200

    add Complex struct

---
+++ Csharp-Notes/Complex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Csharp_Notes
+{
+    public struct Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex
+            {
+                Real = c1.Real + c2.Real,
+                Imaginary = c1.Imaginary + c2.Imaginary
+            };
+        }
+    }   
+}
--- Csharp-Notes/Program.cs
+++ Csharp-Notes/Program.cs
@@ -7,6 +7,10 @@ namespace Csharp_Notes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Complex a = new Complex() { Real = 1, Imaginary = 2 };
+            Complex b = new Complex() { Real = 4, Imaginary = 8 };
+            Complex c = a + b;
+            Console.WriteLine(c.Real);
         }
     }
 }
